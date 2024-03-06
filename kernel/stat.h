b42d4dd675ef22faa90236d6158cb0edc2a755b5
root <root@LAPTOP-PM55CMHB>
2024-03-06 11:11:48 -0600

    add typedefine in stat.h

--- kernel/stat.h
+++ kernel/stat.h
@@ -1,12 +1,13 @@
+#include "types.h"
 #define T_DIR 1    // Directory
 #define T_FILE 2   // File
 #define T_DEVICE 3 // Device
 
 struct stat
 {
-    int dev;            // File system's disk device
-    unsigned int ino;   // Inode number
-    short type;         // Type of file
-    short nlink;        // Number of links to file
-    unsigned long size; // Size of file in bytes
+    int dev;     // File system's disk device
+    uint ino;    // Inode number
+    short type;  // Type of file
+    short nlink; // Number of links to file
+    uint64 size; // Size of file in bytes
 };
