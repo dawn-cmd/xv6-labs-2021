3dbd7089709e07e2b9c7c76d18b0fc33fddf5c97
HuaiYuan Jing <ls.hyjing@gmail.com>
2024-03-07 19:18:19 -0600

    Remove commented out code in xargs.c

--- user/xargs.c
+++ user/xargs.c
@@ -43,7 +43,6 @@ int main(int argc, char *argv[])
     strcpy(offset, readline());
     while (strcmp(offset, ""))
     {
-        // printf("%s\n", offset);
         args[argc - 1] = offset;
         if (fork() == 0)
         {
